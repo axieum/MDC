394aa2f8be2ca094a900338efaca777299c2be7c
Axieum <7577851+Axieum@users.noreply.github.com>
2019-10-06 17:54:39 +1000

    Figured out TOML config tables and object conversion
    * Need to suss out how to inject these objects into the ForgeConfigSpec

--- src/main/java/me/axieum/mcmod/mdc/Config.java
+++ src/main/java/me/axieum/mcmod/mdc/Config.java
@@ -1,13 +1,15 @@
 package me.axieum.mcmod.mdc;
 
+import com.electronwill.nightconfig.core.conversion.ObjectConverter;
 import com.electronwill.nightconfig.core.file.CommentedFileConfig;
 import com.electronwill.nightconfig.core.io.WritingMode;
+import me.axieum.mcmod.mdc.api.ChannelConfig;
 import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.fml.common.Mod;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
-@Mod.EventBusSubscriber
 public class Config
 {
     private static final ForgeConfigSpec.Builder COMMON_BUILDER = new ForgeConfigSpec.Builder();
@@ -16,6 +18,9 @@ public class Config
 
     private static final String CATEGORY_GENERAL = "general";
     public static ForgeConfigSpec.ConfigValue<String> BOT_TOKEN;
+
+    private static final String CATEGORY_CHANNEL = "channel";
+    public static ForgeConfigSpec.ConfigValue<List<List<ChannelConfig>>> CHANNELS;
 
     // Define configuration schema
     static {
@@ -27,6 +32,10 @@ public class Config
                                   .define("bot.token", "");
 
         COMMON_BUILDER.pop();
+
+        // CHANNELS
+        CHANNELS = COMMON_BUILDER.comment("Channel configurations")
+                                 .define(CATEGORY_CHANNEL, new ArrayList<>());
 
         // Publish config
         COMMON_CONFIG = COMMON_BUILDER.build();
@@ -48,5 +57,16 @@ public class Config
 
         configData.load();
         spec.setConfig(configData);
+
+        ChannelsConfig ccs = new ObjectConverter().toObject(configData, ChannelsConfig::new);
+        // TODO: Figure out how to put this `ccs.channels` list into the CHANNELS ConfigValue
+        ccs.channels.forEach(c -> MDC.LOGGER.debug(c.toString()));
+    }
+
+    public static class ChannelsConfig
+    {
+        public List<ChannelConfig> channels;
+
+        public ChannelsConfig() {}
     }
 }
